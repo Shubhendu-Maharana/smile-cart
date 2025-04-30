3f256a376b19ffd1cc263c8f77f80a5e42cb3619
Shubhendu-Maharana <maharanasubhendu3@gmail.com>
2025-04-30 18:36:43 +0530

    Added cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cart from "components/Cart";
 import { PageNotFound } from "components/commons";
 import { Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
@@ -8,6 +9,7 @@ import ProductList from "./components/ProductList";
 
 const App = () => (
   <Switch>
+    <Route exact component={Cart} path={routes.cart} />
     <Route exact component={Product} path={routes.products.show} />
     <Route exact component={ProductList} path={routes.products.index} />
     <Redirect exact from={routes.root} to={routes.products.index} />
