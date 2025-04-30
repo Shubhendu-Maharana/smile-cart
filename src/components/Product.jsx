82c59c2f441f0b41f041fa920ec3bff1588e6e53
Shubhendu-Maharana <maharanasubhendu3@gmail.com>
2025-04-30 15:33:38 +0530

    Added Carousel component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,27 @@
+import { Typography } from "neetoui";
+
+import Carousel from "./Carousel";
+import { IMAGE_URLS } from "./constants";
+
 const Product = () => (
   <div className="px-6 pb-6">
     <div>
-      <p className="py-2 text-4xl font-semibold">Infinix INBOOK</p>
-      <hr className="border-2 border-black" />
+      <Typography className="py-2 text-4xl font-semibold" style="h1">
+        Infinix INBOOK
+      </Typography>
+      <hr className="neeto-ui-border-black border-2" />
     </div>
     <div className="mt-6 flex gap-4">
       <div className="w-2/5">
-        <img
-          alt="Product"
-          className="h-64 w-10/12"
-          src="https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg"
-        />
+        <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
       </div>
       <div className="w-3/5 space-y-4">
-        <p>
+        <Typography>
           Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year Warranty.
-        </p>
-        <p>MRP: $395.97</p>
-        <p className="font-semibold">Offer price: $374.43</p>
-        <p className="font-semibold text-green-600">6% off</p>
+        </Typography>
+        <Typography>MRP: $395.97</Typography>
+        <Typography className="font-semibold">Offer price: $374.43</Typography>
+        <Typography className="font-semibold text-green-600">6% off</Typography>
       </div>
     </div>
   </div>
